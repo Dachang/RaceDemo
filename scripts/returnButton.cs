8c384d683176b7332d3f8a76c3f927fea1a18b39
Ocean <dachang221@126.com>
2014-02-11 21:31:47 +0800

    [Car] 赛道一 车辆全齐

---
+++ scripts/returnButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class returnButton : MonoBehaviour {
+
+    private int screenWidth;
+    private int screenHeight;
+
+    public GUISkin mySkin;
+	// Use this for initialization
+	void Start () 
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.skin = mySkin;
+        if (GUI.Button(new Rect(screenWidth - 60, screenHeight - 50, 40, 40), "ESC"))
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
--- scripts/scoreManager.cs
+++ scripts/scoreManager.cs
@@ -62,6 +62,7 @@ public class scoreManager : MonoBehaviour {
 			Time.timeScale = 1.0f;
 		currentCarID = scScript.getCurrentCarID();
 		updateGameState();
+        Debug.Log(roundNum);
 	}
 	
 	void OnGUI()
@@ -75,8 +76,8 @@ public class scoreManager : MonoBehaviour {
 			case 0:
 				if(startTrigger.tag == "raceOne")
 				{
-					textRoundNum = string.Format(roundNum.ToString() + " / 2");
-					if(roundNum == 3) roundCount = 3;
+					textRoundNum = string.Format((roundNum-1).ToString() + " / 1");
+					if(roundNum == 2) roundCount = 3;
 				}
 				else
 				{
@@ -98,9 +99,115 @@ public class scoreManager : MonoBehaviour {
 				}
 				break;
 			case 1:
-				textRoundNum = string.Format(roundNum.ToString() + " / 2");
-				if(roundNum == 3) roundCount = 3;
+                if (startTrigger.tag == "raceOne")
+                {
+                    textRoundNum = string.Format((roundNum - 1).ToString() + " / 1");
+                    if (roundNum == 2) roundCount = 3;
+                }
+                else
+                {
+                    textRoundNum = string.Format(roundNum.ToString() + " / 2");
+                    if (roundNum == 3) roundCount = 3;
+                }
 				break;
+            case 2:
+                if (startTrigger.tag == "raceOne")
+                {
+                    textRoundNum = string.Format((roundNum - 1).ToString() + " / 1");
+                    if (roundNum == 2) roundCount = 3;
+                }
+                else
+                {
+                    textRoundNum = string.Format(roundNum.ToString() + " / 2");
+                    if (roundNum == 3) roundCount = 3;
+                }
+                break;
+            case 3:
+                if (startTrigger.tag == "raceOne")
+                {
+                    textRoundNum = string.Format((roundNum - 1).ToString() + " / 1");
+                    if (roundNum == 2) roundCount = 3;
+                }
+                else
+                {
+                    textRoundNum = string.Format(roundNum.ToString() + " / 2");
+                    if (roundNum == 3) roundCount = 3;
+                }
+                break;
+            case 4:
+                if (startTrigger.tag == "raceOne")
+                {
+                    textRoundNum = string.Format((roundNum - 1).ToString() + " / 1");
+                    if (roundNum == 2) roundCount = 3;
+                }
+                else
+                {
+                    textRoundNum = string.Format(roundNum.ToString() + " / 2");
+                    if (roundNum == 3) roundCount = 3;
+                }
+                break;
+            case 5:
+                if (startTrigger.tag == "raceOne")
+                {
+                    textRoundNum = string.Format((roundNum - 1).ToString() + " / 1");
+                    if (roundNum == 6) roundCount = 3;
+                }
+                else
+                {
+                    if (roundNum == 1)
+                    {
+                        textRoundNum = string.Format("1 / 2");
+                        roundCount = 1;
+                    }
+                    else if (roundNum == 6)
+                    {
+                        textRoundNum = string.Format("2 / 2");
+                        roundCount = 2;
+                    }
+                    else if (roundNum == 10 || roundNum == 11)
+                    {
+                        //load Result Scene
+                        roundCount = 3;
+                    }
+                }
+                break;
+            case 6:
+                if (startTrigger.tag == "raceOne")
+                {
+                    textRoundNum = string.Format((roundNum - 1).ToString() + " / 1");
+                    if (roundNum == 2) roundCount = 3;
+                }
+                else
+                {
+                    textRoundNum = string.Format(roundNum.ToString() + " / 2");
+                    if (roundNum == 3) roundCount = 3;
+                }
+                break;
+            case 7:
+                if (startTrigger.tag == "raceOne")
+                {
+                    textRoundNum = string.Format((roundNum - 1).ToString() + " / 1");
+                    if (roundNum == 6) roundCount = 3;
+                }
+                else
+                {
+                    if (roundNum == 1)
+                    {
+                        textRoundNum = string.Format("1 / 2");
+                        roundCount = 1;
+                    }
+                    else if (roundNum == 6)
+                    {
+                        textRoundNum = string.Format("2 / 2");
+                        roundCount = 2;
+                    }
+                    else if (roundNum == 10 || roundNum == 11)
+                    {
+                        //load Result Scene
+                        roundCount = 3;
+                    }
+                }
+                break;
 			default:
 				break;
 			}
@@ -109,10 +216,10 @@ public class scoreManager : MonoBehaviour {
 		GUI.Label (new Rect (180,20,500,60), textRoundNum);
 		GUI.skin.label.fontSize = 62;
 		//exitButton
-		if(GUI.Button(new Rect(screenWidth - 60,screenHeight-50,40,40),"ESC"))
-		{
-			Application.Quit();
-		}
+        //if(GUI.Button(new Rect(screenWidth - 60,screenHeight-50,40,40),"ESC"))
+        //{
+        //    Application.Quit();
+        //}
 		//static UI
 		GUI.DrawTexture(new Rect(screenWidth/2 - timeTexture.width/2 - 40,20,timeTexture.width*2/3,timeTexture.height*2/3), timeTexture, ScaleMode.StretchToFill, true, 0);
 		GUI.DrawTexture(new Rect(screenWidth - scoreTexture.width - 80,20,scoreTexture.width*2/3,scoreTexture.height), scoreTexture, ScaleMode.StretchToFill, true, 0);
@@ -126,10 +233,10 @@ public class scoreManager : MonoBehaviour {
 			textRoundNum = string.Format(" ");
 			textScore = string.Format(" ");
 			tScript.clearTimeString();
-			GUILayout.BeginArea(new Rect(screenWidth/2,screenHeight/2 + 100,
+			GUILayout.BeginArea(new Rect(screenWidth/2-128,screenHeight/2 + 100,
 				buttonListWidth,buttonListHeight));
 			GUILayout.BeginHorizontal();
-			if(GUILayout.Button(" ",GUILayout.Height(64),GUILayout.Width(128)))
+			if(GUILayout.Button(" ",GUILayout.Height(128),GUILayout.Width(256)))
 			{
 				scScript.setIsCarSelected(false);
 				scScript.setIsColorSelected(false);
