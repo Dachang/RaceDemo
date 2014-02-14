cd7f03b37c7661e575d459ed989c364bb05a4a26
Ocean <dachang221@126.com>
2014-02-15 05:06:08 +0800

    [Plane]菜单完成，差UI

--- PlaneScripts/planeSelectViewController.cs
+++ PlaneScripts/planeSelectViewController.cs
@@ -30,6 +30,9 @@ public class planeSelectViewController : MonoBehaviour {
 	
 	//sound effect
 	public AudioClip clickSound;
+
+    //page tag
+    private int pageTag = 0;
 	
 	// Use this for initialization
 	void Start () 
@@ -49,47 +52,97 @@ public class planeSelectViewController : MonoBehaviour {
 		if(GUI.Button(new Rect(RACEBTN_ONE_MARGIN_LEFT, RACEBTN_ONE_MARGIN_UP,RACEBTN_ONE_WIDTH,RACEBTN_ONE_HEIGHT), "Plane1"))
 		{
 			audio.PlayOneShot(clickSound);
-			PlayerPrefs.SetInt("planeID",22);
+            if (pageTag == 0)
+                PlayerPrefs.SetInt("planeID", 0);
+            else if (pageTag == 1)
+                PlayerPrefs.SetInt("planeID", 6);
+            else if (pageTag == 2)
+                PlayerPrefs.SetInt("planeID", 12);
+            else if (pageTag == 3)
+                PlayerPrefs.SetInt("planeID", 18);
 			Application.LoadLevel("Scene1");
 		}
 		if(GUI.Button(new Rect(RACEBTN_TWO_MARGIN_LEFT, RACEBTN_ONE_MARGIN_UP,RACEBTN_TWO_WIDTH,RACEBTN_TWO_HEIGHT), "Plane2"))
 		{
 			audio.PlayOneShot(clickSound);
-			PlayerPrefs.SetInt("planeID",1);
+            if (pageTag == 0)
+                PlayerPrefs.SetInt("planeID", 1);
+            else if (pageTag == 1)
+                PlayerPrefs.SetInt("planeID", 7);
+            else if (pageTag == 2)
+                PlayerPrefs.SetInt("planeID", 13);
+            else if (pageTag == 3)
+                PlayerPrefs.SetInt("planeID", 19);
 			Application.LoadLevel("Scene1");
 		}
 		if(GUI.Button(new Rect(RACEBTN_THREE_MARGIN_LEFT, RACEBTN_ONE_MARGIN_UP,RACEBTN_TWO_WIDTH,RACEBTN_TWO_HEIGHT), "Plane3"))
 		{
 			audio.PlayOneShot(clickSound);
-			PlayerPrefs.SetInt("planeID",2);
+            if (pageTag == 0)
+                PlayerPrefs.SetInt("planeID", 2);
+            else if (pageTag == 1)
+                PlayerPrefs.SetInt("planeID", 8);
+            else if (pageTag == 2)
+                PlayerPrefs.SetInt("planeID", 14);
+            else if (pageTag == 3)
+                PlayerPrefs.SetInt("planeID", 20);
 			Application.LoadLevel("Scene1");
 		}
 		if(GUI.Button(new Rect(RACEBTN_ONE_MARGIN_LEFT, RACEBTN_TWO_MARGIN_UP,RACEBTN_TWO_WIDTH,RACEBTN_TWO_HEIGHT), "Plane4"))
 		{
 			audio.PlayOneShot(clickSound);
-			PlayerPrefs.SetInt("planeID",3);
+            if (pageTag == 0)
+                PlayerPrefs.SetInt("planeID", 3);
+            else if (pageTag == 1)
+                PlayerPrefs.SetInt("planeID", 9);
+            else if (pageTag == 2)
+                PlayerPrefs.SetInt("planeID", 15);
+            else if (pageTag == 3)
+                PlayerPrefs.SetInt("planeID", 21);
 			Application.LoadLevel("Scene1");
 		}
 		if(GUI.Button(new Rect(RACEBTN_TWO_MARGIN_LEFT, RACEBTN_TWO_MARGIN_UP,RACEBTN_TWO_WIDTH,RACEBTN_TWO_HEIGHT), "Plane5"))
 		{
 			audio.PlayOneShot(clickSound);
-			PlayerPrefs.SetInt("planeID",4);
+            if (pageTag == 0)
+                PlayerPrefs.SetInt("planeID", 4);
+            else if (pageTag == 1)
+                PlayerPrefs.SetInt("planeID", 10);
+            else if (pageTag == 2)
+                PlayerPrefs.SetInt("planeID", 16);
+            else if (pageTag == 3)
+                PlayerPrefs.SetInt("planeID", 22);
 			Application.LoadLevel("Scene1");
 		}
 		if(GUI.Button(new Rect(RACEBTN_THREE_MARGIN_LEFT, RACEBTN_TWO_MARGIN_UP,RACEBTN_TWO_WIDTH,RACEBTN_TWO_HEIGHT), "Plane6"))
 		{
 			audio.PlayOneShot(clickSound);
-			PlayerPrefs.SetInt("planeID",5);
+            if (pageTag == 0)
+                PlayerPrefs.SetInt("planeID", 5);
+            else if (pageTag == 1)
+                PlayerPrefs.SetInt("planeID", 11);
+            else if (pageTag == 2)
+                PlayerPrefs.SetInt("planeID", 17);
+            else if (pageTag == 3)
+                PlayerPrefs.SetInt("planeID", 23);
 			Application.LoadLevel("Scene1");
 		}
         //switch button
         if (GUI.Button(new Rect(SWITCH_BUTTON_LEFT_MARGIN, SWITCH_BUTTON_MARGIN_HEIGHT, SWITCH_BUTTON_WIDTH, SWITCH_BUTTON_HEIGHT), "<"))
         {
             //prev
+            if (pageTag > 0 && pageTag <= 3)
+            {
+                pageTag--;
+            }
         }
         if (GUI.Button(new Rect(SWITCH_BUTTON_RIGHT_MARGIN, SWITCH_BUTTON_MARGIN_HEIGHT, SWITCH_BUTTON_WIDTH, SWITCH_BUTTON_HEIGHT), ">"))
         {
             //next
+            if (pageTag >= 0 && pageTag < 3)
+            {
+                pageTag++;
+            }
         }
 		//exitButton
 		if(GUI.Button(new Rect(10,screenHeight-60,50,50),"ESC"))
@@ -97,12 +150,6 @@ public class planeSelectViewController : MonoBehaviour {
 			Application.Quit();
 		}
 	}
-	
-	//void OnApplicationQuit()
-	//{
-	//	Application.CancelQuit();
-	//	System.Diagnostics.Process.GetCurrentProcess().Kill();
-	//}
 	
 	void initUIPosition()
 	{
