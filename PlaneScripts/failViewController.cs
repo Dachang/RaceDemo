5bd32fb9a39961fa3b9b678a8d274b6df8201510
Ocean <dachang221@126.com>
2014-02-26 21:12:58 +0800

    [Plane]添加 飞行结束结果显示 界面

---
+++ PlaneScripts/failViewController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class failViewController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.color = new Color(1.0f, 1.0f, 1.0f, .0f);
+        if (GUI.Button(new Rect(650,680,230,120), " "))
+        {
+            //send mail again
+            Application.LoadLevel("Mail");
+        }
+        if (GUI.Button(new Rect(1055, 680, 230, 120), ""))
+        {
+            //goto fly scene
+            Application.LoadLevel("FlyScene");
+        }
+    }
+}
---
+++ PlaneScripts/resultViewController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resultViewController : MonoBehaviour {
+
+    public GUISkin mySkin;
+    private int score;
+	// Use this for initialization
+	void Start () 
+    {
+        score = PlayerPrefs.GetInt("flyResult");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.skin = mySkin;
+        GUI.skin.label.fontSize = 60;
+        GUI.Label(new Rect(745, 390, 500, 140), "你的分数是:" + score.ToString());
+        if (GUI.Button(new Rect(750,640,450,140), " "))
+        {
+            //back to menu
+            Application.LoadLevel(0);
+        }
+    }
+}
