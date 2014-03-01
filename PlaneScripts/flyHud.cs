9b2a1201d895e1b0bc0f9bd87485bbe6e0a5da3a
Ocean <dachang221@126.com>
2014-03-01 11:30:04 +0800

    [Car][Plane]添加提示HUD

---
+++ PlaneScripts/flyHud.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flyHud : MonoBehaviour {
+
+    private int screenWidth;
+    private int screenHeight;
+	// Use this for initialization
+	void Start () 
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.skin.label.fontSize = 18;
+        GUI.Label(new Rect(screenWidth - 370, screenHeight - 50, 350, 30), "提示：小键盘5下降，2爬升，1左转，3右转");
+    }
+}
---
+++ scripts/raceHud.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class raceHud : MonoBehaviour {
+
+    private int screenHeight;
+    private int screenWidth;
+	// Use this for initialization
+	void Start () 
+    {
+        screenHeight = Screen.height;
+        screenWidth = Screen.width;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.skin.label.fontSize = 18;
+        GUI.Label(new Rect(20, screenHeight - 50, 350, 30), "提示：小键盘5前进，2后退，1左转，3右转");
+    }
+}
