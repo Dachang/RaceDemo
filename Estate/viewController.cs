71caa1e7b199f7bf39acfec280c4b2fab372c85a
Ocean <dachang221@126.com>
2014-04-08 23:25:24 +0800

    main scene修改
    1. 左右手指->building出现
    2. 加入renderImage02～07

--- Estate/viewController.cs
+++ Estate/viewController.cs
@@ -6,11 +6,15 @@ public class viewController : MonoBehaviour
     public TOD_Sky sky;
     //GUI Textures
     public Texture2D tabbarBG, screenShot1, screenShot2, screenShot3, screenShot4,
-                     screenShot5, screenShot6, screenShot7, cursorImage, renderImage1, closeBtnImage;
+                     screenShot5, screenShot6, screenShot7, cursorImage, renderImage1,
+                     renderImage2, renderImage3, renderImage4, renderImage5, renderImage6,
+                     renderImage7,closeBtnImage;
+    private Texture2D currentRenderImage;
     private float defaultScreenWidth = 1920f;
     private float defaultScreenHeight = 1080f;
     private int SCREEN_SHOTS_NUM = 7;
     private Texture2D[] screenShots;
+    private Texture2D[] renderImages;
     private Rect[] screenshotRect;
     private Rect closeBtnRect;
     private float SCREEN_SHOTS_GAP = 20f;
@@ -32,19 +36,32 @@ public class viewController : MonoBehaviour
     private int mouseState = 0;
 
     private float CHANGE_TIME_DURATION = 0.05f;
+
+    private GameObject buildingA, buildingB, buildingC, buildingD, buildingE;
+    private bool bATrigger = false;
+    private bool bBTrigger = false;
+    private bool bCTrigger = false;
+    private bool bDTrigger = false;
+    private bool bETrigger = false;
+    private float appearSpeed = 50f;
 	
 	void Start () 
     {
         screenShots = new Texture2D[SCREEN_SHOTS_NUM];
-        screenShots[0] = screenShot1;
-        screenShots[1] = screenShot2;
-        screenShots[2] = screenShot3;
-        screenShots[3] = screenShot4;
-        screenShots[4] = screenShot5;
-        screenShots[5] = screenShot6;
-        screenShots[6] = screenShot7;
+        screenShots[0] = screenShot1; 
+        screenShots[1] = screenShot2; 
+        screenShots[2] = screenShot3; 
+        screenShots[3] = screenShot4; 
+        screenShots[4] = screenShot5; 
+        screenShots[5] = screenShot6; 
+        screenShots[6] = screenShot7; 
         cursorCorrdinate = new Rect(0, 0, 50, 50);
         initScreenShotPosition();
+        buildingA = GameObject.Find("buildingA");
+        buildingB = GameObject.Find("buildingB");
+        buildingC = GameObject.Find("buildingC");
+        buildingD = GameObject.Find("buildingD");
+        buildingE = GameObject.Find("buildingE");
 	}
 	
 	void Update () 
@@ -59,6 +76,7 @@ public class viewController : MonoBehaviour
         calibrateMouse();
         checkLeftHandFingerCount();
         changeTimeThroughGesture();
+        updateBuildingWithTrigger();
 	}
 
     void OnGUI()
@@ -81,7 +99,7 @@ public class viewController : MonoBehaviour
         {
             GUI.DrawTexture(new Rect((1920f / 2 - renderImage1.width / 2) * resizeFactor,
                 (1080f / 2 - renderImage1.height / 2 - 100f) * resizeFactor, renderImage1.width * resizeFactor,
-                renderImage1.height * resizeFactor), renderImage1, ScaleMode.StretchToFill, true, 0);
+                renderImage1.height * resizeFactor), currentRenderImage, ScaleMode.StretchToFill, true, 0);
             GUI.DrawTexture(new Rect((1920f / 2 + renderImage1.width / 2) * resizeFactor,
                 (1080f / 2 - renderImage1.height / 2 - 160f) * resizeFactor, closeBtnImage.width * resizeFactor,
                 closeBtnImage.height * resizeFactor), closeBtnImage, ScaleMode.StretchToFill, true, 0);
@@ -127,6 +145,43 @@ public class viewController : MonoBehaviour
         {
             if (screenshotRect[0].Contains(new Vector3(cursorCorrdinate.x, cursorCorrdinate.y)) && renderImageTrigger == false)
             {
+                currentRenderImage = renderImage1;
+                isMouseSpinning = true;
+                mouseState = 0;
+            }
+            if (screenshotRect[1].Contains(new Vector3(cursorCorrdinate.x, cursorCorrdinate.y)) && renderImageTrigger == false)
+            {
+                currentRenderImage = renderImage2;
+                isMouseSpinning = true;
+                mouseState = 0;
+            }
+            if (screenshotRect[2].Contains(new Vector3(cursorCorrdinate.x, cursorCorrdinate.y)) && renderImageTrigger == false)
+            {
+                currentRenderImage = renderImage3;
+                isMouseSpinning = true;
+                mouseState = 0;
+            }
+            if (screenshotRect[3].Contains(new Vector3(cursorCorrdinate.x, cursorCorrdinate.y)) && renderImageTrigger == false)
+            {
+                currentRenderImage = renderImage4;
+                isMouseSpinning = true;
+                mouseState = 0;
+            }
+            if (screenshotRect[4].Contains(new Vector3(cursorCorrdinate.x, cursorCorrdinate.y)) && renderImageTrigger == false)
+            {
+                currentRenderImage = renderImage5;
+                isMouseSpinning = true;
+                mouseState = 0;
+            }
+            if (screenshotRect[5].Contains(new Vector3(cursorCorrdinate.x, cursorCorrdinate.y)) && renderImageTrigger == false)
+            {
+                currentRenderImage = renderImage6;
+                isMouseSpinning = true;
+                mouseState = 0;
+            }
+            if (screenshotRect[6].Contains(new Vector3(cursorCorrdinate.x, cursorCorrdinate.y)) && renderImageTrigger == false)
+            {
+                currentRenderImage = renderImage7;
                 isMouseSpinning = true;
                 mouseState = 0;
             }
@@ -154,11 +209,37 @@ public class viewController : MonoBehaviour
             case 0:
                 break;
             case 1:
-                renderImageTrigger = true;
+                bATrigger = true;
+                break;
+            case 2:
+                bBTrigger = true;
+                break;
+            case 3:
+                bCTrigger = true;
+                break;
+            case 4:
+                bDTrigger = true;
+                break;
+            case 5:
+                bETrigger = true;
                 break;
             default:
                 break;
         }
+    }
+
+    void updateBuildingWithTrigger()
+    {
+        if (bATrigger && buildingA.transform.position.y <= -50.3f)
+            buildingA.transform.Translate(Vector3.up * Time.deltaTime * appearSpeed);
+        else if (bBTrigger && buildingB.transform.position.y <= -50.3f)
+            buildingB.transform.Translate(Vector3.up * Time.deltaTime * appearSpeed);
+        else if (bCTrigger && buildingC.transform.position.y <= -50.3f)
+            buildingC.transform.Translate(Vector3.up * Time.deltaTime * appearSpeed);
+        else if (bDTrigger && buildingD.transform.position.y <= -50.3f)
+            buildingD.transform.Translate(Vector3.up * Time.deltaTime * appearSpeed);
+        else if (bETrigger && buildingE.transform.position.y <= -50.3f)
+            buildingE.transform.Translate(Vector3.up * Time.deltaTime * appearSpeed);
     }
 
     void changeTimeThroughGesture()
